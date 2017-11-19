26091b795b38f51f3633c9ba8cd3adbd2c13759e
Lera <valeriameryaseva@gmail.com>
2017-11-19 23:13:56 +0400

    Реализовано удаление совета из списка избранных, необходимо сделать обновление списка

--- app/src/main/java/com/example/lera/fgadvice/adapters/FavoritesListAdapter.java
+++ app/src/main/java/com/example/lera/fgadvice/adapters/FavoritesListAdapter.java
@@ -5,9 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.lera.fgadvice.R;
+import com.example.lera.fgadvice.db.DatabaseDAO;
+import com.example.lera.fgadvice.db.DatabaseHelper;
 import com.example.lera.fgadvice.model.Advice;
 
 import java.util.List;
@@ -16,6 +19,8 @@ public class FavoritesListAdapter extends BaseAdapter {
 
     List<Advice> list;
     private Context context;
+
+    private Button deleteAdviceButton;
 
     public FavoritesListAdapter(Context context, List<Advice> list) {
         this.context = context;
@@ -46,6 +51,19 @@ public class FavoritesListAdapter extends BaseAdapter {
         }
         Advice advice = getItem(i);
         ((TextView) view.findViewById(R.id.advice_text)).setText(advice.getText());
+
+        deleteAdviceButton = view.findViewById(R.id.delete_button);
+
+        final int position = i;
+        deleteAdviceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseHelper.deleteFromDB(DatabaseDAO.getDBInstance(context.getApplicationContext()),
+                        Integer.parseInt(list.get(position).getId()));
+                list.remove(position);
+            }
+        });
+
         return view;
     }
 }
